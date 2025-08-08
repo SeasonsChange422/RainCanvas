3c02b1effdd2884279b51392ad1b29c118630de1
1204992313@qq.com <1204992313@qq.com>
2025-08-09 04:20:06 +0800

    [dev] 深拷贝工具

--- core/src/canvas/utils/copy.ts
+++ core/src/canvas/utils/copy.ts
@@ -1,8 +1,41 @@
-export function deepCopyObject(obj){
-    if(obj === null)return
-    let ret = Array.isArray(obj)?[]:{}
-    Object.keys(obj).forEach((key)=>{
-        ret[key] = typeof obj[key]==='object'?deepCopyObject(obj[key]):obj[key]
-    })
-    return ret
+export function deepCopyObject(obj,map=new WeakMap()){
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    if (map.has(obj)) return map.get(obj);
+    const type = Object.prototype.toString.call(obj).slice(8, -1);
+    let clone;
+
+    switch (type) {
+        case 'Date':
+            return new Date(obj);
+        case 'RegExp':
+            return new RegExp(obj);
+        case 'Set':
+            clone = new Set();
+            map.set(obj, clone);
+            obj.forEach(value => clone.add(deepCopyObject(value, map)));
+            return clone;
+        case 'Map':
+            clone = new Map();
+            map.set(obj, clone);
+            obj.forEach((value, key) => clone.set(key, deepCopyObject(value, map)));
+            return clone;
+        case 'Symbol':
+            return Symbol(obj.description);
+        case 'Function':
+            return obj.bind({});
+        case 'Array':
+            clone = [];
+            map.set(obj, clone);
+            obj.forEach((item, i) => clone[i] = deepCopyObject(item, map));
+            return clone;
+        default:
+            clone = Object.create(Object.getPrototypeOf(obj));
+            map.set(obj, clone);
+            Reflect.ownKeys(obj).forEach(key => {
+                clone[key] = deepCopyObject(obj[key], map);
+            });
+            return clone;
+    }
 }
